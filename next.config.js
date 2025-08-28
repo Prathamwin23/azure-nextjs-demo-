911fc0675150a913f339a637b7b739156aa42ef4
unknown <proplayer@gmail.com>
2025-08-28 19:43:07 +0530

    v2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   output: 'standalone',
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-  },
 }
 
 module.exports = nextConfig
